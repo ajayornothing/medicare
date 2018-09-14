32ce29bfe222c6ff4a5192d1aa56a792bc6dd054
ajayornothing <38159719+ajayornothing@users.noreply.github.com>
2018-09-14 12:04:17 +0530

    doctor and patient modules linked

---
+++ medicare.com/html/pharma/patient/script/functions.js
@@ -0,0 +1,237 @@
+jQuery(document).ready(function($) {
+
+    'use strict';
+    
+    // Banner Slider Function
+    $('.health-banner').slick({
+      dots: false,
+      infinite: true,
+      arrows: false,
+      speed: 1000,
+      autoplay: true,
+      fade: true,
+      autoplaySpeed: 2000,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+
+    // Partner Slider Function
+    $('.health-partner-slider').slick({
+      dots: false,
+      infinite: true,
+      arrows: false,
+      speed: 1000,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+    
+    //***************************
+    // Click to Top Button
+    //***************************
+    jQuery('.back-top').on("click", function() {
+        jQuery('html, body').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
+
+});
+
+// Multi-Toggle Navigation
+jQuery(function ($) {
+    'use strict';
+
+    $('body').addClass('js');
+    var $menu = $('#menu'),
+            $menulink = $('.menu-link');
+    $(".health-navigation li").each(function () {
+        var each_li = $(this);
+        if (each_li.find('ul').length > 0) {
+            each_li.append("<span class='has-subnav'><i class='fa fa-angle-down'></i></span>");
+        }
+    });
+    var $menuTrigger = $('.has-subnav');
+
+    $menulink.on("click", function (e) {
+        e.preventDefault();
+        $menulink.toggleClass('active');
+        $menu.toggleClass('active');
+    });
+
+    $menuTrigger.on("click", function (e) {
+        e.preventDefault();
+        var $this = $(this);
+        $this.parent('li').find('ul').toggleClass('active');
+    });
+
+});
+jQuery(window).resize(function(){
+    'use strict';
+    var $ = jQuery;
+    var $menu = $('#menu'),
+    $menulink = $('.menu-link');
+    $menu.removeClass('active');
+    $menulink.removeClass('active');
+});
+
+jQuery('.health-department-progressbar').progressBar({
+    shadow : false,
+    percentage : true,
+    animation : true,
+});
+
+// Counter
+var a = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.counter-value').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 5000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    a = 1;
+  }
+
+});
+
+//***************************
+// FilterAble Function
+//***************************
+jQuery(window).on('load', function() {
+    var $grid = $('.health-project-modren,.health-gallery-simple').isotope({
+      itemSelector: '.element-item',
+      layoutMode: 'fitRows'
+    });
+    // filter functions
+    var filterFns = {
+      // show if number is greater than 50
+      numberGreaterThan50: function() {
+        var number = $(this).find('.number').text();
+        return parseInt( number, 10 ) > 50;
+      },
+      // show if name ends with -ium
+      ium: function() {
+        var name = $(this).find('.name').text();
+        return name.match( /ium$/ );
+      }
+    };
+    // bind filter button click
+    $('.filters-button-group').on( 'click', 'a', function() {
+      var filterValue = $( this ).attr('data-filter');
+      // use filterFn if matches value
+      filterValue = filterFns[ filterValue ] || filterValue;
+      $grid.isotope({ filter: filterValue });
+    });
+    // change is-checked class on buttons
+    $('.filters-button-group').each( function( i, buttonGroup ) {
+      var $buttonGroup = $( buttonGroup );
+      $buttonGroup.on( 'click', 'a', function() {
+        $buttonGroup.find('.is-checked').removeClass('is-checked');
+        $( this ).addClass('is-checked');
+      });
+    });
+});
+
+// When the window has finished loading create our google map below
+google.maps.event.addDomListener(window, 'load', init);
+
+function init() {
+    // Basic options for a simple Google Map
+    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    var mapOptions = {
+        // How zoomed in you want the map to start at (always required)
+        zoom: 11,
+
+        // The latitude and longitude to center the map (always required)
+        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+
+        // How you would like to style the map. 
+        // This is where you would paste any style found on Snazzy Maps.
+        styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}]
+    };
+
+    // Get the HTML DOM element that will contain your map 
+    // We are using a div with id="map" seen below in the <body>
+    var mapElement = document.getElementById('map');
+
+    // Create the Google Map using our element and options defined above
+    var map = new google.maps.Map(mapElement, mapOptions);
+
+    // Let's also add a marker while we're at it
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(40.6700, -73.9400),
+        map: map,
+        title: 'Snazzy!'
+    });
+}
